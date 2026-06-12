93a5ba6f44ddd644c90657e9000ae5a77e07b656
Yinlerens <tpc1919561288@gmail.com>
2026-06-12 11:56:50 +0800

    fix: add service entrypoint

---
+++ cmd/asset-service/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/yinlerens/asset-service/internal/config"
+	"github.com/yinlerens/asset-service/internal/httpapi"
+	"github.com/yinlerens/asset-service/internal/store"
+)
+
+func main() {
+	if err := run(); err != nil {
+		slog.Error("asset service stopped", "error", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	cfg, err := config.Load()
+	if err != nil {
+		return err
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	dataStore, err := store.New(ctx, cfg.DatabaseURL)
+	if err != nil {
+		return err
+	}
+	defer dataStore.Close()
+
+	api := httpapi.New(dataStore, httpapi.Options{
+		InternalToken:  cfg.InternalToken,
+		MaxLedgerLimit: cfg.MaxLedgerLimit,
+	})
+
+	server := &http.Server{
+		Addr:              cfg.Addr,
+		Handler:           api.Handler(),
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	errCh := make(chan error, 1)
+	go func() {
+		slog.Info("asset service listening", "addr", cfg.Addr)
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			errCh <- err
+			return
+		}
+		errCh <- nil
+	}()
+
+	select {
+	case err := <-errCh:
+		return err
+	case <-ctx.Done():
+	}
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	return server.Shutdown(shutdownCtx)
+}
